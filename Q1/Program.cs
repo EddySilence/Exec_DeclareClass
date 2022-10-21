b838e7d6de6f311ec83c1c6024ecfdc543bd3ea4
ispan <ispan@DESKTOP-PAKSETB>
2022-10-21 09:02:02 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Address
+	{
+		public string City { get; set; }
+		public string ZipCode { get; set; }
+		public string Street { get; set; }
+	}
+	class Telephone
+	{
+		public string CountryCode { get; set; }
+		public string AreaCode { get; set; }
+		public string Number { get; set; }
+		public string Ext { get; set; }
+	}
+	class Member
+	{
+		public Address Address { get; set; }
+		public Telephone Telephone { get; set; }
+		public void Register(string Name, string Account, string Password, string confirmPassword, string Email)
+		{
+
+		}
+		public void ForgetPassword(string Name, string Email)
+		{
+
+		}
+		public void Authenticate(string Account,string Password)
+		{
+
+		}
+	}
+}
